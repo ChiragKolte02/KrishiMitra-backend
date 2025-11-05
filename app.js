94eadb521550e5bc30e3317d766108d5cb60e01d
ChiragKolte02 <167008579+ChiragKolte02@users.noreply.github.com>
2025-11-05 16:38:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,12 +22,10 @@ app.use(cookieParser());
 const cors=require("cors")
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://krishimitra-frontend.onrender.com"   // your deployed frontend URL
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  credentials: true
+  origin: ['http://localhost:5174', 'http://localhost:5173'], // Add your frontend URLs
+  credentials: true, // If you're using cookies/auth headers
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use("/receipts", express.static(path.join(__dirname, "receipts")));
