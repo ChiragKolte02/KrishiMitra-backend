e3e5ec632b7e046b62641373f259d707f7467510
ChiragKolte02 <167008579+ChiragKolte02@users.noreply.github.com>
2025-11-05 12:31:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -47,4 +47,5 @@ sequelize.sync({ alter: true })
   .catch((err) => console.error("❌ Sync error:", err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
+
